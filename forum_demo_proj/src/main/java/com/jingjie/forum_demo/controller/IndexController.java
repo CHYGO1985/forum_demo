ad20f898424987c9981e9a0e2c3df7c95d1cd01b
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-01-08 17:40:33 +0800

    Add redirect and exception handler and get cookies.

--- forum_demo_proj/src/main/java/com/jingjie/forum_demo/controller/IndexController.java
+++ forum_demo_proj/src/main/java/com/jingjie/forum_demo/controller/IndexController.java
@@ -1,14 +1,18 @@
 package com.jingjie.forum_demo.controller;
 
 import com.jingjie.forum_demo.model.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.RedirectViewControllerRegistration;
+import org.springframework.web.servlet.view.RedirectView;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.*;
 
 /**
  *
@@ -72,5 +76,67 @@ public class IndexController {
        return "template";
     }
 
-    // get info regardign header and
+    // get info regardign header and cookies
+    // two ways of getting cookies : 1) from HttpServletRequest 2) from @CookieValue
+    // response -->  HttpServletResponse, request --> HttpServletRequest
+    @RequestMapping (path = {"/request"}, method = {RequestMethod.GET})
+    @ResponseBody
+    public String request(Model model, HttpServletResponse response,
+                          HttpServletRequest request,
+                          HttpSession httpSession,
+                          @CookieValue("JSESSIONID") String sessionId) {
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("COOKIE_VALUE: " + sessionId);
+        builder.append(request.getMethod() + "<br>");
+        builder.append(request.getQueryString() + "<br>");
+        builder.append(request.getPathInfo() + "<br>");
+
+        Enumeration<String> headerName = request.getHeaderNames();
+
+        while (headerName.hasMoreElements()) {
+            String name = headerName.nextElement();
+            builder.append(name + ": " + request.getHeader(name) + "<br>");
+        }
+
+        if (request.getCookies() != null) {
+            for(Cookie cookie : request.getCookies()) {
+                builder.append("Cookie: " + cookie.getName() + " value: " + cookie.getValue());
+            }
+        }
+
+        return builder.toString();
+    }
+
+
+    // use RedirectView to redirect a visit (used to switch between sites for mobile and PC)
+    @RequestMapping (path = {"/redirect/{code}"}, method = {RequestMethod.GET})
+    public RedirectView redirect (@PathVariable("code") int code){
+
+        RedirectView redir = new RedirectView("/", true);
+        if (code == 301) {
+            redir.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
+        }
+
+        return redir;
+    }
+
+    // Handle Exception
+    // 1) define a exception handler 2) throw a illegalexception in controller
+    @ExceptionHandler
+    @ResponseBody
+    public String errorMessage(Exception exp) {
+
+        return  "Error: " + exp.getMessage();
+    }
+
+    @RequestMapping (path = {"/admin"}, method = {RequestMethod.GET})
+    @ResponseBody
+    public String getAdmin(@RequestParam("key") int key) {
+
+        if (key != 1)
+            throw new IllegalArgumentException("Invalid ID for admin.");
+
+        return "Admin: " + key;
+    }
 }
