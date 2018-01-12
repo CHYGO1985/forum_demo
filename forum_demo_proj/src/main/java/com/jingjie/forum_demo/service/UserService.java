41bb44544d8bfc89b7e3460f9cddb44d3a03cd2f
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-01-13 00:01:27 +0800

    Add login relation class. 1) Add MD5 method 2) Add register method in UserService

--- forum_demo_proj/src/main/java/com/jingjie/forum_demo/service/UserService.java
+++ forum_demo_proj/src/main/java/com/jingjie/forum_demo/service/UserService.java
@@ -2,8 +2,12 @@ package com.jingjie.forum_demo.service;
 
 import com.jingjie.forum_demo.dao.UserDao;
 import com.jingjie.forum_demo.model.User;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -19,13 +23,44 @@ public class UserService {
     @Autowired
     UserDao userDao;
 
-    public User getUserViaId(int id) {
+    public User getUserViaId (int id) {
 
         return userDao.getUserViaId(id);
     }
 
-    public User getUserViaName(String name) {
+    public User getUserViaName (String name) {
 
         return userDao.getUserViaName(name);
     }
+
+    public Map<String, Object> register (String username, String password) {
+
+        Map<String, Object> map = new HashMap<>();
+
+        // check whether the username is null
+        if (StringUtils.isBlank(username) == true) {
+
+            map.put("msg", "The user name cannot be NULL!");
+            return map;
+        }
+
+        // check whether the password is null
+        if (StringUtils.isBlank(password) == true) {
+
+            map.put("msg", "The password cannot be NULL!");
+            return map;
+        }
+
+        // check whether the username exist already
+        if (userDao.getUserViaName(username) != null) {
+
+            map.put("msg", "The given username" + username + "has been used.");
+            return map;
+        }
+
+        // encrypt password
+
+        return map;
+    }
 }
+
