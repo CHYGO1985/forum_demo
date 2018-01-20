f6454b12e2274a9699f4ca787a338e7d0475239c
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-01-20 23:41:55 +0800

    Add comment model and dao.

---
+++ forum_demo_proj/src/main/java/com/jingjie/forum_demo/dao/CommentDao.java
@@ -0,0 +1,45 @@
+package com.jingjie.forum_demo.dao;
+
+import com.jingjie.forum_demo.model.Comment;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+/**
+ *
+ * The DAO for comment table.
+ *
+ * @author jingjiejiang
+ * @history
+ * 1. Created on Jan 20, 2018
+ */
+@Mapper
+public interface CommentDao {
+
+    String COMMENT_TABLE = "comment";
+    String INSERT_FIELDS = "user_id, created_date, entity_id, entity_type, content, status";
+    String SELECT_FIELDS = "id" + INSERT_FIELDS;
+
+    @Select ({"select " + SELECT_FIELDS + " from " + COMMENT_TABLE + " where id = #{id}"})
+    int getCommentViaId(int id);
+
+    @Select ({"select count(id) from " + COMMENT_TABLE + " where user_id = #{userId}"})
+    int getUserCommentCount(int userId);
+
+    @Insert ({"insert into " + COMMENT_TABLE + " (user_id, created_date, entity_id, entity_type, content, status) " +
+            "value (#{userId}, #{createDate}, #{entityId}), #{entityType}, #{content}, #{status} "})
+    int addComment(Comment comment);
+
+    @Select ({"select " + SELECT_FIELDS + " from " + COMMENT_TABLE + " where entity_id = #{entityId} and entity_type = #{entityType} " +
+            "order by created_date desc"})
+    List<Comment> getCommentViaEntity(@Param("entityId") int entityId,
+                                      @Param("entityType") int entityType);
+
+    @Update ({"update " + COMMENT_TABLE + " set status = #{status} where id = #{id}"})
+    int updateCommentStatus(@Param("id") int id,
+                            @Param("status") String status);
+
+    @Select ({"select " + SELECT_FIELDS + " from " + COMMENT_TABLE + " where entity_id = #{entityId} and entity_type = #{entityType}"})
+    int getCommentCountViaEntity(@Param("entityId") int entityId,
+                                 @Param("entityType") int entityType);
+}
---
+++ forum_demo_proj/src/main/java/com/jingjie/forum_demo/model/Comment.java
@@ -0,0 +1,84 @@
+package com.jingjie.forum_demo.model;
+
+import java.util.Date;
+
+/**
+ *
+ * The model for comment table.
+ *
+ * @author jingjiejiang
+ * @history
+ * 1. Created on Jan 20, 2018
+ *
+ */
+public class Comment {
+
+    private int id;
+    private String content;
+    private int useId;
+    private Date createDate;
+    private int entityId;
+    private int entityType;
+    private int status;
+
+    public Comment () {
+
+        createDate = new Date();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getUseId() {
+        return useId;
+    }
+
+    public void setUseId(int useId) {
+        this.useId = useId;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public int getEntityId() {
+        return entityId;
+    }
+
+    public void setEntityId(int entityId) {
+        this.entityId = entityId;
+    }
+
+    public int getEntityType() {
+        return entityType;
+    }
+
+    public void setEntityType(int entityType) {
+        this.entityType = entityType;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
