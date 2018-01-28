91ef3e706544122bbed4f2d799718bd7341f7f9b
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-01-28 22:55:29 +0800

    Add more methods to JedisAdapter.

--- forum_demo_proj/src/main/java/com/jingjie/forum_demo/util/JedisAdapter.java
+++ forum_demo_proj/src/main/java/com/jingjie/forum_demo/util/JedisAdapter.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
+import java.util.List;
+
 /**
  *
  * The adpater the methods in Jedis.
@@ -92,4 +94,112 @@ public class JedisAdapter implements InitializingBean {
 
         return 0;
     }
+
+    // sismember method
+    public boolean isMember (String key, String value) {
+
+        Jedis jedis = null;
+        try {
+
+            jedis = pool.getResource();
+            return jedis.sismember(key, value);
+        }
+        catch (Exception ex) {
+            logger.error("Fail to check membership: " + ex.getMessage());
+        }
+        finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+
+        return false;
+    }
+
+    // brpop method (blocking list pop up method)
+    public List<String> popFromListTail (String key, int timeout) {
+
+        Jedis jedis = null;
+        try {
+
+            jedis = pool.getResource();
+            return jedis.brpop(timeout, key);
+        }
+        catch (Exception ex) {
+            logger.error("Fail to pop an element from the tail of a list: " +
+                    ex.getMessage());
+        }
+        finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+
+        return null;
+    }
+
+    // lpush method
+    public long addToListHead (String key, String value) {
+
+        Jedis jedis = null;
+        try {
+
+            jedis = pool.getResource();
+            return jedis.lpush(key, value);
+        }
+        catch (Exception ex) {
+            logger.error("Fail to add element to the head of a list: " +
+                    ex.getMessage());
+        }
+        finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+
+        return 0;
+    }
+
+    // lrange
+    public List<String> getListElemsWithRange (String key, int start, int end) {
+
+        Jedis jedis = null;
+        try {
+            jedis = pool.getResource();
+            return jedis.lrange(key, start, end);
+        }
+        catch (Exception ex) {
+            logger.error("Fail to get elems from list:" + ex.getMessage());
+        }
+        finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+
+        return null;
+    }
+
+    // zadd method
+    public long addToSortSet (String key, double score, String value) {
+
+        Jedis jedis = null;
+        try {
+            jedis = pool.getResource();
+            return jedis.zadd(key, score, value);
+        }
+        catch (Exception ex) {
+            logger.error("Fail to add member to sorted set: " +
+                    ex.getMessage());
+        }
+        finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+
+        return 0;
+    }
+
+
 }
